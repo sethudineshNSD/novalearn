d78943b2984b14fde4f77e4b51f854b1ce7673be
sethudinesh <justjackofficial100@email.com>
2025-10-17 20:48:27 +0530

    Switched from bcrypt to bcryptjs for Render compatibility

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import Course from "./models/Course.js"; // ✅ Import Course model
 
 dotenv.config();
